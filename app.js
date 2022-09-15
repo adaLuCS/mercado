a7f03f403849fd224dbfca762a9430510aac5b3d
adalucs <adarelycs@gmail.com>
2022-09-14 20:07:50 -0500

    login y register BASE

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.listen(3030, () => console.log('Levantando un servidor con Express'));
 app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.get ('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get ('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
