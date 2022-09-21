ad88df572aa9a287b44dbc71a6eccb4cb27c6dfb
adalucs <adarelycs@gmail.com>
2022-09-20 20:45:34 -0500

    Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(3030, () => console.log('Levantando un servidor con Express'));
+app.listen(process.env.PORT || 3000, () => console.log('Levantando un servidor con Express'));
 
 app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
